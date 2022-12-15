d4b382064d7a6431cc9c671ef5bcb7f7e5479069
SiHeon No <shtlgjs123@gmail.com>
2022-12-15 21:08:34 +0900

    first commit

---
+++ study.py
@@ -0,0 +1,11 @@
+phone_book = {}
+phone_book["gildong"] = "010-1234-5678"
+phone_book["chans"] = "010-2341-1251"
+
+for key ,value in phone_book.items():
+    print(key, ":", value)
+
+num = [(1,2),3]
+
+for key, value in num:
+    print(key, value) 
